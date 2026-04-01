8ddbbe9de0be9da458bd0f72616a59f1eb1605a2
Elsy Fuentes <elsyfuentes@Elsys-MacBook-Air.local>
2026-04-01 15:05:54 -0400

    Changed database set up to include the array of data

--- models/Player.js
+++ models/Player.js
@@ -4,17 +4,24 @@ const playerSchema = new mongoose.Schema({
   name: { type: String, required: true },
   user: { type: String, required: true },
   password: { type: String, required: true },
+
   age: Number,
   position: String,
   team: String,
-  attendance: Number,
-  playTime: Number,
-  PAC: Number,
-  SHO: Number,
-  PAS: Number,
-  DRI: Number,
-  DEF: Number,
-  PHY: Number
+
+  logs: [
+    {
+      date: { type: Date, default: Date.now },
+      attendance: Boolean,
+      playTime: Number,
+     PAC: { type: Number, min: 0, max: 100 },
+     SHO: { type: Number, min: 0, max: 100 },
+     PAS: { type: Number, min: 0, max: 100 },
+     DRI: { type: Number, min: 0, max: 100 },
+     DEF: { type: Number, min: 0, max: 100 },
+     PHY: { type: Number, min: 0, max: 100 }
+    }
+  ]
 });
 
 module.exports = mongoose.model("Player", playerSchema);
